a9424e5045960f766a2eea55d0a8b4ee2d04214b
Sultan Md. Alnur <98782302+Alnur17@users.noreply.github.com>
2025-09-13 15:44:23 +0600

    added some file here

--- lib/app/modules/login/views/login_view.dart
+++ lib/app/modules/login/views/login_view.dart
@@ -1,22 +1,100 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'package:get/get.dart';
 
+import '../../../../common/app_color/app_colors.dart';
+import '../../../../common/app_images/app_images.dart';
+import '../../../../common/app_text_style/styles.dart';
+import '../../../../common/size_box/custom_sizebox.dart';
+import '../../../../common/widgets/custom_button.dart';
+import '../../../../common/widgets/custom_textfield.dart';
 import '../controllers/login_controller.dart';
 
-class LoginView extends GetView<LoginController> {
+class LoginView extends StatefulWidget {
   const LoginView({super.key});
+
+  @override
+  State<LoginView> createState() => _LoginViewState();
+}
+
+class _LoginViewState extends State<LoginView> {
+  final LoginController loginController = Get.put(LoginController());
+
+  final TextEditingController emailTEController = TextEditingController();
+  final TextEditingController passwordTEController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('LoginView'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'LoginView is working',
-          style: TextStyle(fontSize: 20),
+      backgroundColor: AppColors.white,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20.w),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              sh60,
+              Text(
+                'Login to your Account',
+                style: h2.copyWith(
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+              sh12,
+              Text(
+                'It is quick and easy to log in. Enter your email and password below.',
+                style: h4.copyWith(color: AppColors.grey),
+              ),
+              sh40,
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('Email', style: h4),
+                  sh8,
+                  CustomTextField(
+                    hintText: 'Your email',
+                    containerColor: AppColors.white,
+                    controller: emailTEController,
+                  ),
+                  const SizedBox(height: 12),
+                  Text('Password', style: h4),
+                  sh8,
+                  CustomTextField(
+                    sufIcon: Image.asset(
+                      AppImages.eyeClose,
+                      scale: 4,
+                    ),
+                    hintText: '**********',
+                    containerColor: AppColors.white,
+                    controller: passwordTEController,
+                  ),
+                ],
+              ),
+
+              sh24,
+              // Obx(
+              //       () {
+              //     return loginController.isLoading.value == true
+              //         ? CustomLoader(color: AppColors.white)
+              //         :
+              CustomButton(
+                text: 'Login',
+                onPressed: () {
+                  //Get.to(()=> DashboardView());
+                  // loginController.userLogin(
+                  //   email: emailTEController.text,
+                  //   password: passwordTEController.text,
+                  // );
+                },
+                imageAssetPath: AppImages.arrowRightNormal,
+                gradientColors: AppColors.buttonColor,
+                //   );
+                // },
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'package:get/get.dart';
 
 import 'app/routes/app_pages.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(
-    GetMaterialApp(
-      title: "Application",
-      initialRoute: AppPages.INITIAL,
-      getPages: AppPages.routes,
+    ScreenUtilInit(
+      designSize: const Size(375, 812),
+      child: GetMaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: "Technical Assessment",
+        initialRoute: AppPages.INITIAL,
+        getPages: AppPages.routes,
+      ),
     ),
   );
 }
