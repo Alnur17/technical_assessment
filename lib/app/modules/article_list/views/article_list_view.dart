7ad335e9ca391a730b5e8523c5540fac4f872d1c
Alnur17 <mr.nur30@gmail.com>
2025-09-15 00:10:03 +0600

    get storage added and article list design

--- lib/app/modules/article_list/views/article_list_view.dart
+++ lib/app/modules/article_list/views/article_list_view.dart
@@ -1,23 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'package:get/get.dart';
+import 'package:technical_assessment/common/app_color/app_colors.dart';
 
+import '../../../../common/app_text_style/styles.dart';
 import '../controllers/article_list_controller.dart';
 
 class ArticleListView extends GetView<ArticleListController> {
   const ArticleListView({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppColors.mainColor,
       appBar: AppBar(
+        backgroundColor: AppColors.mainColor,
         title: const Text('ArticleListView'),
         centerTitle: true,
       ),
-      body: const Center(
-        child: Text(
-          'ArticleListView is working',
-          style: TextStyle(fontSize: 20),
-        ),
+      body: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 20).r,
+            //padding: EdgeInsets.symmetric(horizontal: 12, vertical: 16).r,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(12),
+              color: AppColors.silver,
+              border: Border.all(color: AppColors.borderColor),
+            ),
+            child: ListTile(
+              onTap: () {},
+              leading: Text(
+                '1',
+                style: h5,
+              ),
+              title: Text(
+                'This is for the title',
+                style: h5,
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+              ),
+              subtitle: Text('This is going to be the subtitle'),
+            ),
+            // Row(
+            //   children: [
+            //     Text(
+            //       '1',
+            //       style: h5,
+            //     ),
+            //     sw12,
+            //     Text(
+            //       'This is the title ',
+            //       style: h5,
+            //       maxLines: 1,
+            //       overflow: TextOverflow.ellipsis,
+            //     ),
+            //   ],
+            // ),
+          ),
+        ],
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -2,18 +2,26 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 
 import 'app/routes/app_pages.dart';
+import 'common/app_constant/app_constant.dart';
+import 'common/helper/local_store.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await GetStorage.init();
+
+  var userToken = LocalStorage.getData(key: AppConstant.token) ?? '';
+
   runApp(
     ScreenUtilInit(
       designSize: const Size(375, 812),
       child: GetMaterialApp(
         debugShowCheckedModeBanner: false,
         title: "Technical Assessment",
-        initialRoute: AppPages.INITIAL,
+        // Just because i don't want to use the splash view right now
+        initialRoute: userToken.isNotEmpty ? Routes.ARTICLE_LIST : Routes.LOGIN,
         getPages: AppPages.routes,
       ),
     ),
